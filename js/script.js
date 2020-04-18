c53c055458ca74eeaf1ec777bcc1c5d780837167
AppGeek <sed.mohsen.moosavi@gmail.com>
2020-04-18 18:28:36 +0430

    Preserve path default classes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 
 function animate (el) {
-    console.log(el)
     var pathes = el.querySelectorAll('path')
+    var defaultClasses = []
     for (var i = 0; i < pathes.length; i++) {
+        defaultClasses.push(pathes[i].className.baseVal)
         pathes[i].className.baseVal = 'path transparent-path drawing-path'
     }
 
     setTimeout(function () {
         for(var i = 0; i < pathes.length; i++) {
-            pathes[i].className.baseVal = 'path'
+            pathes[i].className.baseVal = defaultClasses[i] + 'path'
         }
     }, 1500)
 }
